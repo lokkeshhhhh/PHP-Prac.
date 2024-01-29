da4cd942148f0f41490587db85bb6f21b80d45b5
Lokesh Kumawat <lokesh8250kumawat@gmail.com>
2024-01-29 21:53:12 +0530

    Update first.php

--- first.php
+++ first.php
@@ -1,46 +1,49 @@
 <?php
 
-// echo "hello";
+echo "hello";
 
-// $LOKESH = 19;
+$LOKESH = 19;
 
-// $lOKESH = '19';
+$lOKESH = '19';
 
-// if($lOKESH==$LOKESH){
-//     echo "Lokesh";
-// }
+if($lOKESH==$LOKESH){
+    echo "Lokesh";
+}
 
 
-// $lOKESH===$LOKESH ?echo "A": echo "B";
+$lOKESH===$LOKESH ?echo "A": echo "B";
 
-// echo str_word_count("lokesh kumawat is a good boy!");
+echo str_word_count("lokesh kumawat is a good boy!");
 
-// echo var_dump((array) 23)
+echo var_dump((array) 23)
 
-// $array = array(1,2,3,4);
-// // $array[5] = 9;
+$array = array(1,2,3,4);
+// $array[5] = 9;
 
-// for($i=0;$i<count($array);$i++){
-//     echo $array[$i];
-// }
-// $i=1;
-// do {
+for($i=0;$i<count($array);$i++){
+    echo $array[$i];
+}
+$i=1;
+do {
     
-//     if($i==5){ $i++; break;}
-// echo "<br> $i";
-// $i++;
-// } while ($i<=10);
+    if($i==5){ $i++; break;}
+echo "<br> $i";
+$i++;
+} while ($i<=10);
 
 
 
 
-// $array = array(2, 3, 4, 5);
+$array = array(2, 3, 4, 5);
 
-// foreach ($array as $x => $y){
-//     echo " <div style='color:darkgreen;'>$x : $y <br><br></div>" ;
-// }
+foreach ($array as $x => $y){
+    echo " <div style='color:darkgreen;'>$x : $y <br><br></div>" ;
+}
 
 
+[$a, $b, $c] = array(11, 12, 13);
+
+echo $c;
 
 
 
